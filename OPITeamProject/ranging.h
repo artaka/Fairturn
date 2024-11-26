2176042abc1ae37c4a9065ab0e2c7e55fd67f124
artaka <fefsert@gmail.com>
2024-11-27 00:28:52 +0300

    GetSum and GetSurnames func added

---
+++ OPITeamProject/ranging.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> getSurnames(map <string, vector <int>> members);
+map <string, vector <int>> getSum(map <string, vector <int>> members, vector<string> surnames);
