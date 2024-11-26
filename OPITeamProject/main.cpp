3de6e8a17ecefae23a4588d2cc93b33a6f9b7a0a
shadebod <gittigwork@gmail.com>
2024-11-27 01:23:26 +0300

    Deleting the contents of the main.cpp file

--- OPITeamProject/main.cpp
+++ OPITeamProject/main.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
 
-int main() {
-	std::cout << "Hello, good :) team!";
-	
-	std::cout << "Hello. делаю измение в своеё ветке и мёржу";
-	for (int i = 0; i < 2; i++)
-	{
-		std::cout << "?\n";
-	}
-	return 0;
-}
