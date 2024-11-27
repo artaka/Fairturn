4f412f4a1a52ca29159053667de4e6dd9675f4b9
artaka <fefsert@gmail.com>
2024-11-28 02:38:07 +0300

    added ranging func

--- OPITeamProject/main.cpp
+++ OPITeamProject/main.cpp
@@ -4,13 +4,15 @@
 int main() {
 	//testing
 	map <string, vector <int>> Members = {
-	{"Чел1",{5,5,0}},
-	{"Чел2",{5,10,0}},
-	{"Чел3",{1,10,10}}
+	{"Аня",{5,5,0}},//1
+	{"Ваня",{5,10,10}},//3
+	{"Саня",{1,10,10}}//2
 	};
 
 	vector<string> surnames = getSurnames(Members);
 	
 	map <string, vector <int>> res = getSum(Members, surnames);
+
+	vector<pair<string, vector<int>>> out = ranging(res, surnames);
 	cout << "End";
 }
--- OPITeamProject/ranging.h
+++ OPITeamProject/ranging.h
@@ -5,4 +5,7 @@
 using namespace std;
 
 vector<string> getSurnames(map <string, vector <int>> members);
+
 map <string, vector <int>> getSum(map <string, vector <int>> members, vector<string> surnames);
+
+vector<pair<string, vector<int>>> ranging(map <string, vector <int>> members, vector<string> surnames);
