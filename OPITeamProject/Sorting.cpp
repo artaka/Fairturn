fdf07d33b9a96d73835746cf5caf4e4d6d543320
artemdol12 <artemdol12@mail.ru>
2024-11-28 11:41:14 +0300

    mod Sorting

--- OPITeamProject/Sorting.cpp
+++ OPITeamProject/Sorting.cpp
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-﻿#include <iostream>
-=======
+#include <iostream>
 #include <iostream>
 #include <map>
->>>>>>> 9bf3cea53a74633d26539c459c7f79f154a5d5e5
 #include <vector>
 #include <algorithm>
 #include <string>
@@ -13,15 +10,12 @@ using namespace std;
 // Функция для сортировки и вывода данных
 void printSortedData(vector<pair<string, vector<int>>>& sortedMembers) {
     setlocale(LC_CTYPE, "Russian");
-<<<<<<< HEAD
 
     // Сортируем по фамилии (по первому элементу пары)
     sort(sortedMembers.begin(), sortedMembers.end(), 
         [](const pair<string, vector<int>>& a, const pair<string, vector<int>>& b) {
             return a.first < b.first;  // Сортируем по имени (фамилии)
         });
-=======
->>>>>>> 9bf3cea53a74633d26539c459c7f79f154a5d5e5
 
     // Номер строки для вывода
     int lineNumber = 1;
