2b56a8fbc7006e4c3222df7a120122650cb75e82
Artem Myshkovets <43890646+artaka@users.noreply.github.com>
2024-12-03 00:39:18 +0300

    Update main.cpp

--- OPITeamProject/main.cpp
+++ OPITeamProject/main.cpp
@@ -1,18 +1,1 @@
-#include <iostream>
-#include "ranging.h"
 
-int main() {
-	//testing
-	map <string, vector <int>> Members = {
-	{"Аня",{5,5,0}},//1
-	{"Ваня",{5,10,10}},//3
-	{"Саня",{1,10,10}}//2
-	};
-
-	vector<string> surnames = getSurnames(Members);
-	
-	map <string, vector <int>> res = getSum(Members, surnames);
-
-	vector<pair<string, vector<int>>> out = ranging(res, surnames);
-	cout << "End";
-}
