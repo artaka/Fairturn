512c0c75f7c57938bf45e91882fe31ffe247c01d
m0loko <gittigwork@gmail.com>
2025-09-20 19:12:04 +0300

    minor changes with the addition of a new parameter

--- OPITeamProject/input.cpp
+++ OPITeamProject/input.cpp
@@ -3,32 +3,46 @@
 #include <map>
 #include <string>
 #include "input.h"
-#include <stdio.h>
 #include <Windows.h>
 
 using namespace std;
-map <string, vector<int>> dataEntry() {
-	setlocale(LC_ALL, "RU");
-	SetConsoleCP(1251);
-	map <string, vector<int>> membersListWithNumbers;
-	int num, index;
-	string sym = " ";
-	cout << "Enter the number of participants: "; cin >> num;
-	cout << "Enter the details of each participant one by one in the following format: <Last name> <number of labs taken> <number of the lab you are taking> <whether you were eligible to take the test at the previous lesson(yes - 10, no - 0)> AND \";\" IN THE END!\n";
-	for (int i = 0; i < num; i++) {
-		vector<int> numbers(3);
-		string str, surname;
-		cout << i + 1 << ". "; getline(cin, str, ';');
-		index = str.find_first_of(sym);
-		surname = str.substr(1, index - 1);
-		str.erase(0, (index + 1));
-		for (int i = 0; i < 3; i++) {
-			index = str.find_first_of(sym);
-			numbers[i] = stoi(str.substr(0, index));
-			str.erase(0, (index + 1));
-		}
-		membersListWithNumbers[surname] = numbers;
-	}
-	system("cls");
-	return membersListWithNumbers;
+
+map<string, vector<int>> dataEntry() {
+    setlocale(LC_ALL, "RU");
+    SetConsoleCP(1251);
+
+    map<string, vector<int>> membersListWithNumbers;
+    int num, index;
+    string sym = " ";
+
+    cout << "Enter the number of participants: ";
+    cin >> num;
+
+    int currentLab;
+    cout << "Enter current lab number: ";
+    cin >> currentLab;
+
+    cout << "Enter details for each participant in format:\n";
+    cout << "<LastName> <labsPassed> <submittingLab> <attended(yes-10 / no-0)> ;\n";
+
+    cin.ignore(); // очистка буфера
+
+    for (int i = 0; i < num; i++) {
+        vector<int> numbers(4);
+        string str, surname;
+        cout << i + 1 << ". ";
+        getline(cin, str, ';');
+        index = str.find_first_of(sym);
+        surname = str.substr(0, index);
+        str.erase(0, (index + 1));
+        for (int j = 0; j < 3; j++) {
+            index = str.find_first_of(sym);
+            numbers[j] = stoi(str.substr(0, index));
+            str.erase(0, (index + 1));
+        }
+        numbers[3] = currentLab; // добавляем текущую лабу
+        membersListWithNumbers[surname] = numbers;
+    }
+    system("cls");
+    return membersListWithNumbers;
 }
